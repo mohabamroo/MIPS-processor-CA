edfe55dc1382919f99e634bbe28df93163abd88a
MOHAB <mohabamr1@gmail.com>
2017-04-15 14:00:22 +0200

    msh fahm 7agaaaa

---
+++ alu.v
@@ -0,0 +1,48 @@
+module ALU(in1, in2, aluop, outRes, zeroflag);
+	input [31:0] in1;
+	input [31:0] in2;
+	input [2:0] aluop;
+
+	// variables (manipulated) should be reg,
+	// but wires are just outputs (not manipulated)
+	output reg [31:0] outRes;
+	output reg zeroflag;
+
+	always @ (in1, in2, aluop)
+	begin
+		if(in1 == in2)
+		zeroflag = 1;
+		else
+		zeroflag = 0;
+		// these controls are customizable
+		// I didnt find a strict set
+		// 000 means add or sth like that
+		case(aluop)
+			0: outRes = in1 + in2;	// add
+			1: outRes = in1 - in2;	// sub
+			2: outRes = in1 & in2;	// and
+			3: outRes = in1 | in2;	// or
+			4: outRes = in1 < in2;	// slt
+			5: outRes = ~(in1 | in2);	// nor
+		endcase
+	end
+endmodule
+
+
+module test();
+	reg [31:0] x, y;
+	reg [2:0] sel;
+	wire [31:0] res;
+	wire zeroflag;
+
+
+	// #x means @ time x, do kza
+	initial begin
+		x = 5; y = 5; sel = 0;
+		#10 $display("The resut is %d", res);
+		$display("flag: %d", zeroflag);
+	end
+
+	always @ (x, y, sel);
+		ALU test(x, y, sel, res, zeroflag);
+endmodule
---
+++ control.v
@@ -0,0 +1,108 @@
+module control(opcode, RegDest, Branch, MemRead,
+	MemtoReg, MemWrite, ALUOp, ALUSrc, RegWrite);
+	input [5:0] opcode;
+	output reg RegDest, Branch, MemRead,
+	MemtoReg, MemWrite, ALUSrc, RegWrite;
+	output reg [2:0] ALUOp;
+
+	// needed set of instructions
+	// Arithmetic: add, sub, addi
+	// Load/Store: lw, sw
+	// Logic: and, or, sll, srl, andi, ori
+	// Control flow: beq, bne
+	// Comparison: slt
+
+	// ALUOp problem?
+	always @ (opcode)
+	begin
+	case(opcode)
+		// R-format: add, sub, and, or, sll, srl, slt
+		6'b000000: begin
+			RegDest <= 1'b1;
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0;
+			MemWrite <= 1'b0;
+			ALUSrc <= 1'b0;
+			RegWrite <= 1'b1;
+			ALUOp <= 3'b010; // what for?
+		end
+		// addi
+		6'b001000: begin
+			RegDest <= 1'b0;
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0;
+			MemWrite <= 1'b0;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b1;
+			ALUOp <= 3'b000; // add in alu?
+		end
+		// andi
+		6'b001100: begin
+			RegDest <= 1'b0;
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0;
+			MemWrite <= 1'b0;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b1;
+			ALUOp <= 3'b010; // and in alu?
+		end
+		// ori
+		6'b001000: begin
+			RegDest <= 1'b0;
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0;
+			MemWrite <= 1'b0;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b1;
+			ALUOp <= 3'b011; // add in alu?
+		end
+		// lw
+		6'b001000: begin
+			RegDest <= 1'b0;
+			Branch <= 1'b0;
+			MemRead <= 1'b1;
+			MemtoReg <= 1'b1;
+			MemWrite <= 1'b0;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b1;
+			ALUOp <= 3'b000; // from table in lec
+		end
+		// sw
+		6'b101011: begin
+			RegDest <= 1'b0; // don't care
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0; // don't care
+			MemWrite <= 1'b1;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b0;
+			ALUOp <= 3'b000; // from table in lec
+		end
+		// beq
+		6'b000100: begin
+			RegDest <= 1'b0; // don't care
+			Branch <= 1'b1;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0; // don't care
+			MemWrite <= 1'b0; 
+			ALUSrc <= 1'b0;
+			RegWrite <= 1'b0;
+			ALUOp <= 3'b001; // from table in lec
+		end
+		// bne
+		6'b000101: begin
+			RegDest <= 1'b0; // don't care
+			Branch <= 1'b0;
+			MemRead <= 1'b0;
+			MemtoReg <= 1'b0; // don't care
+			MemWrite <= 1'b1;
+			ALUSrc <= 1'b1;
+			RegWrite <= 1'b0;
+			ALUOp <= 3'b001; // from table in lec
+		end
+	end
+endmodule
